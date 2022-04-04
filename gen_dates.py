ba82c3c3b6154b9b960970c8e7cbd3e209a0488e
Gvido Bērziņš <85738497+gvido-lokalise@users.noreply.github.com>
2022-04-04 10:24:40 +0300

    Fix gen_dates.py and update gitignore

--- gen_dates.py
+++ gen_dates.py
@@ -5,16 +5,18 @@ Summary:
 from datetime import date, timedelta
 
 
-def gen_dates(delta):
+def gen_dates(start_date, end_date):
+    delta = end_date - start_date  # returns timedelta
     for i in range(delta.days + 1):
         day = start_date + timedelta(days=i)
         print(day.strftime("%Y%m%d"))
 
+
 def main():
     start_date = date(2022, 1, 1)
     end_date = date(2022, 3, 14)  # perhaps date.now()
-    delta = end_date - start_date  # returns timedelta
+    gen_dates(start_date, end_date)
+
 
 if __name__ == "__main__":
     main()
-
