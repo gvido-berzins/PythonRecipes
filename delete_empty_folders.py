9d30478568841316a78a912dd909f1be70e431cd
Gvido Bērziņš <gvido.berzins@testdevlab.com>
2022-06-18 20:54:53 +0300

    feat: Add recursive folder deletion

---
+++ delete_empty_folders.py
@@ -0,0 +1,22 @@
+""" 
+Summary: 
+    Delete empty folders recursively, including empty parent directories
+Description:
+    Found in this helpful stackoverflow answer https://stackoverflow.com/a/65624165
+"""
+
+
+def delete_empty_folders(root):
+    """Delete empty folders and empty folder structures recursively"""
+    deleted = set()
+    for current_dir, subdirs, files in os.walk(root, topdown=False):
+        still_has_subdirs = any(
+            subdir
+            for subdir in subdirs
+            if os.path.join(current_dir, subdir) not in deleted
+        )
+        if not any(files) and not still_has_subdirs:
+            os.rmdir(current_dir)
+            deleted.add(current_dir)
+
+    return deleted
